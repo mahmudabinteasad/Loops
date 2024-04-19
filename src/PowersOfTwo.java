8d3d3b1ce4ac601600b249a134139f522f51c465
samiaasad <samiaasad604@gmail.com>
2024-04-19 19:45:19 +0600

    E6.1(c)

---
+++ src/PowersOfTwo.java
@@ -0,0 +1,8 @@
+public class PowersOfTwo {
+    public static void main(String[] args) {
+        System.out.println("Powers of 2 from 2^0 up to 2^20:");
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(Math.pow(2, i));
+        }
+    }
+}
