a56b3c1a5c147d0bbe27e4d68841795f6fd2e28c
samiaasad <samiaasad604@gmail.com>
2024-04-19 19:54:33 +0600

    E6.2(d)

---
+++ src/AdjacentDuplicates.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AdjacentDuplicates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter integers separated by space: ");
+
+        int prev = scanner.nextInt();
+        while (scanner.hasNextInt()) {
+            int current = scanner.nextInt();
+            if (current == prev) {
+                System.out.print(current + " ");
+            }
+            prev = current;
+        }
+
+        scanner.close();
+    }
+}
